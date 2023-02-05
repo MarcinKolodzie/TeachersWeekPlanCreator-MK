b1ae4dcb64c1ef5b5865ae690dcd7022d51fd28c
Marcin Kołodziej <marcin1.kolodziej@gmail.com>
2023-02-05 17:09:56 +0100

    'Loader' with 'Spinner' added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import Loader from './Loader'
 
-export const App = () => {
-  return (
-    <div>
-      MK APP
-    </div>
-  )
+export class App extends React.Component {
+  state = {
+    isLoading: false,
+    route: 'WELCOME', // 'CHOSE-SUBJECTS' or 'CREATE-WEEK-PLAN'
+    subject: ''
+  }
+
+  render () {
+    const { isLoading } = this.state
+
+    return (
+      <div>
+        <h1>MK APP</h1>
+        {
+          isLoading ?
+            <Loader />
+            :
+            null
+        }
+      </div>
+    )
+  }
 }
 
 export default App
